e91f2ff74073c23244091e3c705db03e8fe2437e
Yak <you@example.com>
2016-09-30 11:54:27 -0400

    BANG BANG Trouble shooted main.js file (path issue)

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,20 @@ var cardOne = "Queen";
 var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
+
+if(cardOne === cardTwo || cardTwo === cardOne)
+	{
+		alert("You found a match!");
+	}
+else if(cardOne !== cardTwo || cardTwo !== cardOne)
+	{
+		alert("Sorry, try again.");
+	}
+if(cardThree === cardFour || cardFour === cardThree)
+	{
+		alert("You found a match!");
+	}
+else if(cardThree !== cardFour || cardFour !== cardThree)
+	{
+		alert("Sorry, try again.");
+	}
