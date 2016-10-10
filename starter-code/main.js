e5d04ced91db058fe03807d131e2e11245e48d9d
Yianni <Yianni21@gmail.com>
2016-10-10 18:27:55 -0400

    Wrote game functions and logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,9 @@
+/**
 var cardOne = "Queen";
 var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
+**/
 
 /** if(cardOne === cardTwo || cardTwo === cardOne)
 	{
@@ -19,14 +21,60 @@ else if(cardThree !== cardFour || cardFour !== cardThree)
 	{
 		alert("Sorry, try again.");
 	}**/
+var cards = ['Queen', 'Queen', 'King', 'King'];
+var cardsInPlay = [];
 
-var createNewCard = function(x) {
-	for (var i = 0; i < x; i++) {
-		var newCard = document.createElement('div');
-		newCard.className = 'card';
-		document.getElementById('game-board').appendChild(newCard);	
+
+var createNewCard = function() {
+	for (var i = 0; i < cards.length; i++) {
+			var newCard = document.createElement('div');
+			newCard.className = 'card';
+			newCard.setAttribute('data-card', cards[i]);
+			newCard.addEventListener('click', isTwoCards);
+			document.getElementById('game-board').appendChild(newCard);
 	}
 }
-createNewCard(4);
 
+
+var isMatch = function () {
+	
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+
+		alert("You found a match!");
+	}
+
+	else {
+		alert("Sorry, try again.");
+	}
+
+	for (var x = -1, y = 0; x <= cardsInPlay.length; x++, y++) {
+	
+	document.querySelectorAll('.card')[y].innerHTML = " ";	
+	
+	}
+
+
+}
+var isTwoCards = function () {
+	
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'King') {
+		
+		this.innerHTML = "<img src='King.png'>";
+	} 
+
+	else {
+		this.innerHTML = "<img src='Queen.png'>";
+	}
+
+	if (cardsInPlay.length === 2) {
+
+		isMatch(cardsInPlay);
+  		cardsInPlay = [];
+
+	}
+}
+
+createNewCard();
 
